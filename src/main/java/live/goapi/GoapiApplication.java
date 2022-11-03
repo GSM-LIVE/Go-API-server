7a9571f4261fe48d5135be30e7a35f1c6535cf61
KimHuiMang <piyrw9754@gmail.com>
2022-11-03 17:35:03 +0900

    project setting

---
+++ src/main/java/live/goapi/GoapiApplication.java
@@ -0,0 +1,13 @@
+package live.goapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/live/goapi/GoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package live.goapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
