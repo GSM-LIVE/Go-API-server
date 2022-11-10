587ba9e540c19fe1b610d108344dddf8a4d3a2f8
김태오 <taeo0328@gmail.com>
2022-11-10 20:56:48 +0900

    domain클래스 작성

---
+++ src/main/java/live/goapi/domain/Teacher.java
@@ -0,0 +1,29 @@
+package live.goapi.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "teacher_id", nullable = false)
+    private Integer teacherId;
+
+    @Column(name = "teacher_name" , nullable = false , unique = true)
+    private String teacherName;
+
+    @Column(name = "subject" , nullable = false, unique = true)
+    private String subject;
+
+}
