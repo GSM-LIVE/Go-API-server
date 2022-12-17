cb5ca8ddaae3b675f651a23f20a6f4e008a42421
KimHuiMang <piyrw9754@gmail.com>
2022-12-17 15:26:24 +0900

    :sparkles: jwtRequestFilter 작성
    jwtRequestFilter 작성

--- src/main/java/live/goapi/global/filter/JwtRequestFilter.java
+++ src/main/java/live/goapi/global/filter/JwtRequestFilter.java
@@ -1,12 +1,47 @@
 package live.goapi.global.filter;
 
+import live.goapi.global.security.exception.TokenNotValidException;
+import live.goapi.global.security.jwt.JwtTokenProvider;
+import live.goapi.global.security.jwt.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+    private final JwtTokenProvider tokenProvider;
+    private final JwtProperties jwtProperties;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = request.getHeader("Authorization");
+
+        if(!Objects.isNull(accessToken)) {
+            tokenProvider.extractAllClaims(accessToken, jwtProperties.getAccessSecret());
+
+            if (!tokenProvider.getTokenType(accessToken, jwtProperties.getAccessSecret()).equals("ACCESS_TOKEN"))
+                throw new TokenNotValidException("Token is not valid");
+
+            String email = tokenProvider.getUserEmail(accessToken, jwtProperties.getAccessSecret());
+            registerSecurityContext(request, email);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void registerSecurityContext(HttpServletRequest request, String email) {
+        UsernamePasswordAuthenticationToken authenticationToken = tokenProvider.authentication(email);
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+    }
 
 }
