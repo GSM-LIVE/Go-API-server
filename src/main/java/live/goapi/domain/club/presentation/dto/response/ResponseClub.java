57259c8e71c75496e71a25865dca50a8f3556be4
KimHuiMang <piyrw9754@gmail.com>
2022-12-20 20:06:04 +0900

    전공동아리 전체 조회 기능 ㅊ ㅜ가

---
+++ src/main/java/live/goapi/domain/club/presentation/dto/response/ResponseClub.java
@@ -0,0 +1,14 @@
+package live.goapi.domain.club.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseClub {
+    private String clubName;
+}
