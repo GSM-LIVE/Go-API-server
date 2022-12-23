9faec693ec95dc9baff067ff089d5265d81c0871
KimHuiMang <piyrw9754@gmail.com>
2022-12-23 12:28:16 +0900

    survey dto 작성

---
+++ src/main/java/live/goapi/domain/survey/presentation/SurveyController.java
@@ -0,0 +1,12 @@
+package live.goapi.domain.survey.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/survey")
+public class SurveyController {
+
+}
---
+++ src/main/java/live/goapi/domain/survey/service/WriteSurveyService.java
@@ -0,0 +1,7 @@
+package live.goapi.domain.survey.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WriteSurveyService {
+}
