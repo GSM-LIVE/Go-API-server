c94f81d614872e63ae13ffd3c8c03a62484772a7
KimHuiMang <piyrw9754@gmail.com>
2022-12-25 13:51:25 +0900

    :sparkles: 학생 전체 조회 기능 추가
    학생 전체 조회 api 추가

--- src/main/java/live/goapi/domain/student/presentation/StudentInfoController.java
+++ src/main/java/live/goapi/domain/student/presentation/StudentInfoController.java
@@ -1,9 +1,6 @@
 package live.goapi.domain.student.presentation;
 
-import live.goapi.domain.student.presentation.dto.request.RequestStudentClub;
-import live.goapi.domain.student.presentation.dto.request.RequestStudentMajor;
-import live.goapi.domain.student.presentation.dto.request.RequestStudentName;
-import live.goapi.domain.student.presentation.dto.request.RequestStudentNumber;
+import live.goapi.domain.student.presentation.dto.request.*;
 import live.goapi.domain.student.presentation.dto.response.ResponseStudent;
 import live.goapi.domain.student.service.StudentInfoService;
 import lombok.RequiredArgsConstructor;
@@ -23,25 +20,31 @@ public class StudentInfoController {
 
     private final StudentInfoService studentInfoService;
 
-    @PostMapping("/info/name")
+    @PostMapping
+    public ResponseEntity<List<ResponseStudent>> getAllStudents(@RequestBody @Valid RequestAllStudent request) {
+        List<ResponseStudent> response = studentInfoService.getAllStudents(request);
+        return new ResponseEntity<>(response , HttpStatus.OK);
+    }
+
+    @PostMapping("/name")
     public ResponseEntity<ResponseStudent> getStudentInfoByName(@RequestBody @Valid RequestStudentName request) {
         ResponseStudent response = studentInfoService.getStudentInfoByStudentName(request);
         return new ResponseEntity<>(response , HttpStatus.OK);
     }
 
-    @PostMapping("/info/number")
+    @PostMapping("/number")
     public ResponseEntity<ResponseStudent> getStudentInfoByNumber(@RequestBody @Valid RequestStudentNumber request) {
         ResponseStudent response = studentInfoService.getStudentInfoByStudentNumber(request);
         return new ResponseEntity<>(response , HttpStatus.OK);
     }
 
-    @PostMapping("/info/major")
+    @PostMapping("/major")
     public ResponseEntity<List<ResponseStudent>> getStudentInfoByMajor(@RequestBody @Valid RequestStudentMajor request) {
         List<ResponseStudent> response = studentInfoService.getStudentsInfoByMajor(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PostMapping("/info/club")
+    @PostMapping("/club")
     public ResponseEntity<List<ResponseStudent>> getStudentInfoByClub(@RequestBody RequestStudentClub request) {
         List<ResponseStudent> response = studentInfoService.getStudentsByClub(request);
         return new ResponseEntity<>(response, HttpStatus.OK);
