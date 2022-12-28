95f9115d9747a3a914dd09e9112fb5821d191870
KimHuiMang <piyrw9754@gmail.com>
2022-12-28 17:45:09 +0900

    :sparkles: API 인증키 재조회 기능 추가
    인증키 재조회 기능

--- src/main/java/live/goapi/domain/api_key/presentation/ApiKeyController.java
+++ src/main/java/live/goapi/domain/api_key/presentation/ApiKeyController.java
@@ -1,14 +1,13 @@
 package live.goapi.domain.api_key.presentation;
 
 import live.goapi.domain.api_key.presentation.dto.response.ApiKeyResponse;
+import live.goapi.domain.api_key.service.LookUpApiKeyService;
 import live.goapi.domain.api_key.service.RandomApiKeyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiKeyController {
 
     private final RandomApiKeyService randomApiKeyService;
+    private final LookUpApiKeyService lookUpApiKeyService;
+
+    @GetMapping("/api-key")
+    public ResponseEntity<ApiKeyResponse> getApiKey() {
+        ApiKeyResponse response = lookUpApiKeyService.lookUpApiKey();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/api-key/name")
+    public ResponseEntity<ApiKeyResponse> getApiKeyByMemberName(@RequestBody String name) {
+        ApiKeyResponse response = lookUpApiKeyService.lookUpApiKeyByMemberName(name);
+        return ResponseEntity.ok().body(response);
+    }
 
     @PostMapping
     public ResponseEntity<ApiKeyResponse> getNewApiKey() {
